53b2e0f7e3d1f0d16dc21069baebb140f1852129
Hyunggi Chang <hyunggi.chang95@gmail.com>
2023-04-11 17:02:18 +0900

    Add nvidia-docker

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -85,5 +85,20 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt-get -y update
 sudo apt-get -y install gh
 
+# Nvidia-docker
+sudo systemctl --now enable docker
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get -y update
+
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
 # Finished
 echo "Now reboot system to apply changes"
